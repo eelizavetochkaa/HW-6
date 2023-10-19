79a86bd3c09d28352371946f44ada6b408dff3ae
Huawei <Huawei@LAPTOP-Q85GDL68>
2023-10-19 18:34:34 +0300

    Добавьте файлы проекта.

---
+++ ДЗ 6/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ_6
+{
+    abstract class Attraction
+    {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public Attraction(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public abstract void Start();
+        public abstract void Stop();
+    }
+
+    class WaterSlide : Attraction
+    {
+        public bool IsOpen { get; private set; }
+
+        public WaterSlide(string name, int capacity) : base(name, capacity)
+        {
+            IsOpen = false;
+        }
+
+        public override void Start()
+        {
+            Console.WriteLine("Водная горка запущена!");
+            IsOpen = true;
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine("Водная горка остановлена!");
+            IsOpen = false;
+        }
+
+        public void ChangeCapacity(int newCapacity)
+        {
+            if (newCapacity > 0)
+            {
+                Capacity = newCapacity;
+                Console.WriteLine($"Изменена вместимость водной горки на {newCapacity} человек");
+            }
+            else
+            {
+                Console.WriteLine("Некорректное значение вместимости!");
+            }
+        }
+    }
+
+    class Pool : Attraction
+    {
+        public bool IsOpen { get; private set; }
+
+        public Pool(string name, int capacity) : base(name, capacity)
+        {
+            IsOpen = false;
+        }
+
+        public override void Start()
+        {
+            Console.WriteLine("Бассейн открыт!");
+            IsOpen = true;
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine("Бассейн закрыт!");
+            IsOpen = false;
+        }
+
+        public void ChangeTemperature(int newTemperature)
+        {
+            Console.WriteLine($"Температура бассейна изменена на {newTemperature} градусов");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Тема - аквапарк");
+            Console.WriteLine("Введите название водной горки:");
+            string waterSlideName = Console.ReadLine();
+
+            Console.WriteLine("Введите вместимость водной горки:");
+            bool flag = true;
+            bool a = int.TryParse(Console.ReadLine(), out int waterSlideCapacity);
+            if (a)
+            {
+                flag = false;
+            }
+            else
+            {
+                Console.WriteLine("Неверный ввод - необходимо ввести целое число");
+            }
+
+            WaterSlide waterSlide = new WaterSlide(waterSlideName, waterSlideCapacity);
+
+            Console.WriteLine("Введите название бассейна:");
+            string poolName = Console.ReadLine();
+
+            Console.WriteLine("Введите вместимость бассейна:");
+            bool flag1 = true;
+            bool a1 = int.TryParse(Console.ReadLine(), out int poolCapacity);
+            if (a1)
+            {
+                flag1 = false;
+            }
+            else
+            {
+                Console.WriteLine("Неверный ввод - необходимо ввести целое число");
+            }
+            Pool pool = new Pool(poolName, poolCapacity);
+
+            waterSlide.Start();
+            Console.WriteLine($"Вместимость водной горки: {waterSlide.Capacity}");
+
+            Console.WriteLine("Введите новую вместимость водной горки:");
+            bool flag2 = true;
+            bool a2 = int.TryParse(Console.ReadLine(), out int newWaterSlideCapacity);
+            if (a2)
+            {
+                flag2 = false;
+            }
+            else
+            {
+                Console.WriteLine("Неверный ввод - необходимо ввести целое число");
+            }
+            waterSlide.ChangeCapacity(newWaterSlideCapacity);
+
+            waterSlide.Stop();
+
+            pool.Start();
+            Console.WriteLine($"Вместимость бассейна: {pool.Capacity}");
+
+            Console.WriteLine("Введите температуру бассейна:");
+            bool flag3 = true;
+            bool a3 = int.TryParse(Console.ReadLine(), out int newPoolTemperature);
+            if (a3)
+            {
+                flag3 = false;
+            }
+            else
+            {
+                Console.WriteLine("Неверный ввод - необходимо ввести целое число");
+            }
+
+            pool.ChangeTemperature(newPoolTemperature);
+
+            pool.Stop();
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ тумаков 7/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тумаков_7
+{
+    public enum AccountType { tecuchy, sberegatelny }
+    class account
+    {
+
+
+        public account(int number, float balance, AccountType types)
+        {
+            this.number = number;
+            this.balance = balance;
+            Type = types;
+        }
+        private enum types
+        {
+            tecuchy,
+            sberegatelny
+        }
+
+        private int number { get; set; }
+        public float balance { get; set; }
+
+        private AccountType Type { get; set; }
+        public override string ToString() => $"Номер счёта - {number}, Баланс - {balance}, Тип - {Type}";
+
+
+
+
+    }
+    public enum AccountType2 { tecuchy, sberegatelny }
+    class account2
+    {
+        int numberaccount;
+        static int kolvoaccountov = 0;
+
+        public account2(float balance, AccountType2 types)
+        {
+            this.numberaccount = kolvoaccountov;
+            kolvoaccountov += 1;
+            this.balance = balance;
+            Type = types;
+        }
+        private enum types
+        {
+            tecuchy,
+            sberegatelny
+        }
+
+        public float balance { get; set; }
+
+        private AccountType2 Type { get; set; }
+
+        public override string ToString() => $"Номер счёта - {numberaccount}, Баланс - {balance}, Тип - {Type}";
+
+
+        public static void Snyat(int schet1, int balance1)
+        {
+            if (balance1>=schet1)
+            {
+                Console.WriteLine("Можно снять деньги");
+                Console.WriteLine($"Новый счёт {balance1 - schet1}");
+            }
+            else
+            {
+                Console.WriteLine("Деньги нельзя снять : не хватает средств");
+            }
+        }
+
+    }
+    class Building
+    {
+        private static int lastBuildingNumber = 0;
+
+        public int BuildingNumber { get; private set; }
+        public double Height { get; private set; }
+        public int Floors { get; private set; }
+        public int Apartments { get; private set; }
+        public int Entrances { get; private set; }
+
+        public Building(double height, int floors, int apartments, int entrances)
+        {
+            BuildingNumber = GenerateBuildingNumber();
+            Height = height;
+            Floors = floors;
+            Apartments = apartments;
+            Entrances = entrances;
+        }
+
+        private int GenerateBuildingNumber()
+        {
+            return ++lastBuildingNumber;
+        }
+
+        public double CalculateFloorHeight()
+        {
+            return Height / Floors;
+        }
+
+        public int CalculateApartmentsPerEntrance()
+        {
+            return Apartments / Entrances;
+        }
+
+        public int CalculateApartmentsPerFloor()
+        {
+            return Apartments / Floors;
+        }
+
+        public void PrintBuildingInfo()
+        {
+            Console.WriteLine("Номер дома: " + BuildingNumber);
+            Console.WriteLine("Высота: " + Height + " метров");
+            Console.WriteLine("Этажей: " + Floors);
+            Console.WriteLine("Квартир: " + Apartments);
+            Console.WriteLine("Подъездов: " + Entrances);
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Упражнение 7.1");
+                Console.WriteLine("Программа создаёт класс счет в банке с закрытыми полями");
+                AccountType accountType = AccountType.tecuchy;
+                Console.WriteLine(accountType);
+                account bank = new account(12317771, 120_000, AccountType.sberegatelny);
+                Console.WriteLine(bank);
+
+                Console.WriteLine("Упражнение 7.2");
+                Console.WriteLine("Программа создаёт класс счет в банке с закрытыми полями");
+                AccountType2 accountType1 = AccountType2.tecuchy;
+                Console.WriteLine(accountType);
+                account2 bank1 = new account2(120_000, AccountType2.sberegatelny);
+                Console.WriteLine(bank1);
+
+                Console.WriteLine("Упражнение 7.3");
+                Console.WriteLine("Введите, сколько денег вы хотите снять");
+                bool flag = true;
+                bool success = int.TryParse(Console.ReadLine(), out int schet);
+                if (success)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод - необходимо ввести целое число");
+                }
+                account2.Snyat(schet, 120000);
+
+                Console.WriteLine("Домашняя работа 7.1");
+                Building building = new Building(50.5, 10, 100, 5);
+                building.PrintBuildingInfo();
+
+                Console.WriteLine("Высота этажа: " + building.CalculateFloorHeight() + " метров");
+                Console.WriteLine("Квартир в подъезде: " + building.CalculateApartmentsPerEntrance());
+                Console.WriteLine("Квартир на этаже: " + building.CalculateApartmentsPerFloor());
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
